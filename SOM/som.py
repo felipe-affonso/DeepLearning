a50b0a2983c7e56e74ba0a17539928ea52ce1583
Felipe Affonso <felipe-affonso@hotmail.com>
2018-09-27 11:28:54 -0300

    Finishing the SOM

--- SOM/som.py
+++ SOM/som.py
@@ -23,3 +23,38 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0,1))
 X = sc.fit_transform(X)
 
+#treinando o SOM
+# utilizaremos o MiniSom
+from minisom import MiniSom
+som = MiniSom(x= 10, y = 10, input_len = 15, sigma=1.0, learning_rate = 0.5)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 100)
+
+#visualizando os resultados
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5, # colocando o marcador no centro do quadrado
+         markers[y[i]],
+         markeredgecolor = colors[y[i]],
+         markerfacecolor = 'None',
+         markersize = 10,
+         markeredgewidth = 2)
+show()
+    
+#Encontrando as fraudes
+mappings = som.win_map(X)
+frauds = mappings[(1,2)]
+frauds = sc.inverse_transform(frauds)
+
+    
+    
+    
+
+
