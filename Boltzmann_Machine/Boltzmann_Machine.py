632d87a33eb8b633dd6c771d7e419384ba1e13e5
Felipe Affonso <felipe-affonso@hotmail.com>
2018-10-09 02:31:31 -0300

    RBM Model Ready

--- Boltzmann_Machine/Boltzmann_Machine.py
+++ Boltzmann_Machine/Boltzmann_Machine.py
@@ -65,3 +65,71 @@ test_set[test_set == 2] = 0
 test_set[test_set >= 3] = 1
 
 
+# Criando a arquitetura da rede neural
+
+class RBM():
+    def __init__(self, nv, nh): #nv = nós visiveis, nh = hidden nodes
+        self.W = torch.randn(nh, nv) #pesos aleatorios
+        self.a = torch.randn(1, nh) #biad pros nós escondidos
+        self.b = torch.randn(1, nv) # bias pros nós visiveis
+        
+    def sample_h(self, x): # grupos de nós escondidos
+        wx = torch.mm(x, self.W.t()) #produto de dois vetores torch
+        activation = wx + self.a.expand_as(wx)  #sigmoid
+        p_h_given_v = torch.sigmoid(activation)
+        return p_h_given_v, torch.bernoulli(p_h_given_v)
+    
+    def sample_v(self, y): #grupos de nós visiveis
+        wy = torch.mm(y, self.W) #produto de dois vetores torch
+        activation = wy + self.b.expand_as(wy)  #sigmoid
+        p_h_given_h = torch.sigmoid(activation)
+        return p_h_given_h, torch.bernoulli(p_h_given_h)
+    """
+    v0 = vetor contendo as notas de todos os filmes de um usuario
+    vk = nós visiveis obtidos após k interações
+    ph0 = vetor de probabilidades, que na primeira interação irao valer 1
+    phk = probabilidades dos nós escondidos após k interações
+    """
+    def train(self, v0, vk, ph0, phk): # contrastive divergence
+        self.W += (torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)).t()
+        self.b += torch.sum((v0-vk), 0)
+        self.a += torch.sum((ph0 - phk), 0)
+        
+
+#criando o objeto RBM
+nv = len(training_set[0]) #nós de entrada
+nh = 100 #numero de nós escondidos
+batch_size = 100 #tamanho do batch (após quantas interações os pesos serao atualizados)
+
+rbm = RBM(nv, nh)
+
+#treinando o RBM
+nb_epoch = 10
+for epoch in range(1, nb_epoch+1):
+    train_loss = 0
+    s = 0.
+    for id_user in range (0, nb_users - batch_size, batch_size):
+        vk = training_set[id_user:id_user+batch_size]
+        v0 = training_set[id_user:id_user+batch_size] #vetor de notas que os vk usuarios ja deram uma nota
+        ph0,_ = rbm.sample_h(v0) #probabilidades iniciais
+        
+        #for para os k passos da contrastive divergence
+        for k in range(10):
+             _,hk = rbm.sample_h(vk) #pega os visibles nodes e calcula os hidden nodes
+             _,vk = rbm.sample_v(hk) #atualiza os visibles nodes com base nos hidden nodes
+             vk[v0 < 0] = v0[v0<0]
+         
+        phk,_ = rbm.sample_h(vk)
+        rbm.train(v0,vk,ph0,phk)
+        
+        # atualizando o erro
+        train_loss += torch.mean(torch.abs(v0[v0>=0]-vk[v0>=0]))
+        s += 1.
+    print('epoch: '+str(epoch)+' loss: '+str(train_loss/s))
+        
+#testando a RBM
+    
+
+
+
+        
